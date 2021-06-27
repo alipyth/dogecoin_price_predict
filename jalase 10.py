dcabc688dd2a792cdd896c9e68e6a186f2712690
Ali Jahani <81471730+alipyth@users.noreply.github.com>
2021-06-27 12:36:32 +0430

    Update jalase 10.py

--- jalase 10.py
+++ jalase 10.py
@@ -1,81 +1,27 @@
-# class Triangle:
-#     def __init__(self,angle1,angle2,angle3):
-#         self.angle1 = int(angle1)
-#         self.angle2 = int(angle2)
-#         self.angle3 = int(angle3)
-#
-#     def set_angle1(self):
-#         return self.angle1
-#     def set_angle2(self):
-#         return self.angle2
-#     def set_angle3(self):
-#         return self.angle3
-#
-#     def get_angles(self, angle1, angle2, angle3):
-#         if angle1+angle2+angle3 != 180:
-#             raise ValueError("180 nist")
-#         else:
-#             print('nist')
-#
-# myriangle = Triangle(100,10,70)
-# tiangle1 = myriangle.set_angle1()
-# tiangle2 = myriangle.set_angle2()
-# tiangle3 = myriangle.set_angle3()
-# print(myriangle.get_angles(tiangle1,tiangle2,tiangle3))
-#
-
-
-
-
-# class Shape():
-#     def __init__(self):
-#         self.height = 0
-#         self.ghaede = 0
-#     def mashat(self):
-#         pass
-#     def mohit(self):
-#         pass
-#
-# class Triangle(Shape):
-#     def masahat(self,height,ghaede):
-#         return height * ghaede / 2
-#
-# class Square(Shape):
-#     def mohit(self):
-#         return self.ghaede * self.ghaede
-#
-#
-# sh = Shape()
-# tr=Triangle()
-# print(tr.masahat(5,10))
-# sq = Square()
-# print(sq.mohit(5))
-
-
-
-
-class Employee():
-    def __init__(self,peronalcode,resume):
-        self.__peronalcode = peronalcode
-        self.__resume=resume
-
-    def __str__(self):
-        super().__init__().__str__()
-
-
-
-class Hourly(Employee):
-    def __init__(self,peronalcode,resume,hz):
-        super().__init__(peronalcode,resume).__str__()
-        self.__hz = hz
-
-
-
-class Full_time(Employee):
-    def __init__(self,peronalcode,resume,hoghogh):
-        super(Full_time, self).__init__(peronalcode,resume)
-        self.__hoghogh = hoghogh
-
-
-
-
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+bitcoin = pd.read_csv('DOGE-USD.csv')
+plt.figure(figsize = (12, 7))
+plt.plot(bitcoin["Date"], bitcoin["Close"], color='goldenrod', lw=2)
+plt.title("Dogecoin Price over time", size=25)
+plt.xlabel("Time", size=20)
+plt.ylabel("$ Price", size=20)
+bitcoin.isnull().sum()
+bitcoin.dropna(inplace=True)
+required_features = ['Open', 'High', 'Low', 'Volume']
+output_label = 'Close'
+x_train, x_test, y_train, y_test = train_test_split(bitcoin[required_features],bitcoin[output_label],test_size = 0.3)
+model = LinearRegression()
+model.fit(x_train, y_train)
+model.score(x_test, y_test)
+future_set = bitcoin.shift(periods=30).tail(30)
+prediction = model.predict(future_set[required_features])
+plt.figure(figsize = (12, 7))
+plt.plot(bitcoin["Date"][-400:-60], bitcoin["Open"][-400:-60], color='goldenrod', lw=2)
+plt.plot(future_set["Date"], prediction, color='deeppink', lw=2)
+plt.title("Doge Price", size=25)
+plt.xlabel("Time", size=20)
+plt.ylabel("$ Price", size=20)
+plt.show()
